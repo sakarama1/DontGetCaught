9d93e6de145b32cf1aff6065c240ca52498ae0d9
sarpermelikertekin <sakarama1@hotmail.com>
2020-11-07 14:33:04 +0100

    Buttons made

---
+++ Dont Get Caught/Assets/OpenLaserDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLaserDoor : MonoBehaviour
+{
+
+    public GameObject laserDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openIt()
+    {
+        laserDoor.SetActive(false);
+    }
+}
