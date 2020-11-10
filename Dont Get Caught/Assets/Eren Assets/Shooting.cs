123b5c09c073687bc0dc6c3f01159b4912d4234d
Eren98 <eoner98@gmail.com>
2020-11-10 20:52:17 +0100

    Player ranged attack implemented

---
+++ Dont Get Caught/Assets/Eren Assets/Shooting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform shootingPoint;
+
+    void ShootEvent()
+    {
+        GameObject clone = Instantiate(bullet, shootingPoint.transform.position, Quaternion.identity);
+        clone.GetComponent<Bullet>().direction = transform.forward;
+        Destroy(clone, 2f);
+    }
+}
